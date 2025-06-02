01746496bc80b71891db900397bd87a3c199af5b
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-06-02 10:42:37 -0300

    update http

--- src/components/ListaDeUsuarios/index.js
+++ src/components/ListaDeUsuarios/index.js
@@ -10,7 +10,7 @@ function ListaDeUsuarios() {
     useEffect(() => {
         const carregarUsuarios = async () => {
             try {
-                const response = await axios.get('https://backend-time-futebol.onrender.com/usuarios')
+                const response = await axios.get('https://revisao-deploy-backend.onrender.com/usuarios')
                 setUsuarios(response.data)
             } catch (error) {
                 alert('Erro ao buscar usuários: ', error)
